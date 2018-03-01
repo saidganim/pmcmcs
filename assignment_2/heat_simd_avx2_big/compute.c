24cdfdd5963fcff73bec85bd114a1ee4abf7ba4c
Saidgani Musaev <cpu808694@gmail.com>
2018-03-01 02:06:21 +0100

    Lets try it :)

--- assignment_2/heat_simd_avx2_big/compute.c
+++ assignment_2/heat_simd_avx2_big/compute.c
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <output.h>
 #include <immintrin.h>
+#include <omp.h>
 
 static unsigned int N,M;
 
@@ -49,15 +50,6 @@ void do_compute(const struct parameters* p, struct results *r)
 	__m256d weighted_neighb_reg;
 	__m256d temp_init_reg;
 	__m256d temp_init_reg_clone;
-	__m256d temp_init_reg_left;
-	__m256d temp_init_reg_right;
-	__m256d temp_init_reg_top;
-	__m256d temp_init_reg_d1;
-	__m256d temp_init_reg_d2;
-	__m256d temp_init_reg_d3;
-	__m256d temp_init_reg_d4;
-	__m256d temp_init_reg_bottom;
-	__m256d temp_init_reg_bottom_clone;
 	__m256d maxdiff_vect[thread_num];
 	__m256d maxdiff_vect2;
 	__m128d tmp_reg;
@@ -94,161 +86,168 @@ void do_compute(const struct parameters* p, struct results *r)
 	(*temp_tmp)[N + 1][M + 1] = (*temp_init)[N + 1][M + 1] = (*temp_init)[N + 1][1];
 
 	gettimeofday(&start, 0);
-	#pragma omp parallel
-	while(iter < p->maxiter && maxdiff > p->threshold){
-		// do computations;
-		#pragma omp barrier
-		maxdiff = 0.0;
-		for(int i = 0 ; i < 2; ++i)
-			maxdiff_vect[i] = _mm256_set1_pd(0.0);
-		// update most left and most right columns( cache suffers )
-		#pragma omp for
-		for(int i = 0; i < N; ++i){
-			(*temp_init)[i + 1][0] = (*temp_init)[i + 1][M]; // move last column to 0's
-			(*temp_init)[i + 1][M + 1] = (*temp_init)[i + 1][1]; // move first column to (M+1)'s
-			(*temp_tmp)[i + 1][0] = (*temp_init)[i + 1][M]; // move last column to 0's
-			(*temp_tmp)[i + 1][M + 1] = (*temp_init)[i + 1][1]; // move first column to (M+1)'s
-		}
+	#pragma omp parallel private(\
+		weighted_neighb_reg, \
+		temp_init_reg, \
+		temp_init_reg_clone, \
+		tmp_reg, \
+		direct_sum, \
+		diag_sum, \
+		cond_reg, \
+		rev_cond_reg, \
+		double_temp_reg\
+	)
+	{
+		__m256d temp_init_reg_left;
+		__m256d temp_init_reg_right;
+		__m256d temp_init_reg_top;
+		__m256d temp_init_reg_d1;
+		__m256d temp_init_reg_d2;
+		__m256d temp_init_reg_d3;
+		__m256d temp_init_reg_d4;
+		__m256d temp_init_reg_bottom;
 
-		// finally start computations
-		#pragma omp for reduction (max: maxdiff)
-		for(int i = 1; i <= N; ++i){
-			int thread_id = omp_get_thread_num();
-			__m256d weighted_neighb_reg;
-			__m256d temp_init_reg;
-			__m256d temp_init_reg_clone;
-			__m256d temp_init_reg_left;
-			__m256d temp_init_reg_right;
-			__m256d temp_init_reg_top;
-			__m256d temp_init_reg_d1;
-			__m256d temp_init_reg_d2;
-			__m256d temp_init_reg_d3;
-			__m256d temp_init_reg_d4;
-			__m256d temp_init_reg_bottom;
-			__m256d temp_init_reg_bottom_clone;
-			__m128d tmp_reg;
-			__m256d direct_sum;
-			__m256d diag_sum;
-			__m256d cond_reg;
-			__m256d rev_cond_reg;
-			__m256d double_temp_reg;
-			for(int j = 1; j <= (M - M % elems_per_iter) ; j += elems_per_iter){
-				cond_reg = _mm256_loadu_pd(&_index_macro(p->conductivity, i - 1, j - 1));
-				rev_cond_reg = _mm256_sub_pd(const1_reg, cond_reg);
 
-				temp_init_reg = _mm256_loadu_pd(&(*temp_init)[i][j]);
-				temp_init_reg_clone = temp_init_reg; //_mm256_loadu_pd(&(*temp_init)[i][j]);
-				temp_init_reg_left = _mm256_loadu_pd(&(*temp_init)[i][j - 1]);
-				temp_init_reg_right = _mm256_loadu_pd(&(*temp_init)[i][j + 1]);
-				temp_init_reg_top = _mm256_loadu_pd(&(*temp_init)[i - 1][j]);
-
-				temp_init_reg_d1 = _mm256_loadu_pd(&(*temp_init)[i - 1][j - 1]);
-				temp_init_reg_d3 = _mm256_loadu_pd(&(*temp_init)[i - 1][j + 1]);
-
-				temp_init_reg_bottom = _mm256_loadu_pd(&(*temp_init)[i + 1][j]);
-				temp_init_reg_bottom_clone = temp_init_reg_bottom; //_mm256_loadu_pd(&(*temp_init)[i + 1][j]);
-
-				temp_init_reg_d4 = _mm256_loadu_pd(&(*temp_init)[i + 1][j + 1]);
-				temp_init_reg_d2 = _mm256_loadu_pd(&(*temp_init)[i + 1][j - 1]);
-
-				direct_sum = _mm256_add_pd(temp_init_reg_left, temp_init_reg_right);
-				direct_sum = _mm256_add_pd(direct_sum, temp_init_reg_top);
-				direct_sum = _mm256_add_pd(direct_sum, temp_init_reg_bottom);
-				direct_sum = _mm256_mul_pd(direct_sum, coef1_reg);
-
-				diag_sum = _mm256_add_pd(temp_init_reg_d1, temp_init_reg_d2);
-				diag_sum = _mm256_add_pd(diag_sum, temp_init_reg_d3);
-				diag_sum = _mm256_add_pd(diag_sum, temp_init_reg_d4);
-				diag_sum = _mm256_mul_pd(diag_sum, coef2_reg);
-
-				diag_sum = _mm256_add_pd(diag_sum, direct_sum);
-				diag_sum = _mm256_mul_pd(diag_sum, rev_cond_reg);
-				temp_init_reg = _mm256_mul_pd(temp_init_reg, cond_reg);
-				temp_init_reg = _mm256_add_pd(temp_init_reg, diag_sum);
-				_mm256_storeu_pd(&(*temp_tmp)[i][j], temp_init_reg);
-				double_temp_reg = _mm256_sub_pd(temp_init_reg, temp_init_reg_clone);
-					maxdiff_vect[thread_id] = _mm256_max_pd(maxdiff_vect[thread_id], _mm256_max_pd(_mm256_sub_pd(_mm256_set1_pd(0.0), double_temp_reg), double_temp_reg));
+		while(iter < p->maxiter && maxdiff > p->threshold){
+			// do computations;
+			#pragma omp barrier
+			maxdiff = 0.0;
+			for(int i = 0 ; i < 2; ++i)
+				maxdiff_vect[i] = _mm256_set1_pd(0.0);
+			// update most left and most right columns( cache suffers )
+			#pragma omp for
+			for(int i = 0; i < N; ++i){
+				(*temp_init)[i + 1][0] = (*temp_init)[i + 1][M]; // move last column to 0's
+				(*temp_init)[i + 1][M + 1] = (*temp_init)[i + 1][1]; // move first column to (M+1)'s
+				(*temp_tmp)[i + 1][0] = (*temp_init)[i + 1][M]; // move last column to 0's
+				(*temp_tmp)[i + 1][M + 1] = (*temp_init)[i + 1][1]; // move first column to (M+1)'s
 			}
 
-			for(int j = (M - M % elems_per_iter) + 1; j <= M ; ++j){
-				double weighted_neighb = dir_nc *
-				( // Direct neighbors
-					(*temp_init)[i - 1][j] + (*temp_init)[i][j - 1] +
-					(*temp_init)[i + 1][j] + (*temp_init)[i][j + 1]
-				) + dig_nc *
-				( // Diagonal neighbors
-					(*temp_init)[i - 1][j - 1] + (*temp_init)[i + 1][j - 1] +
-					(*temp_init)[i - 1][j + 1] + (*temp_init)[i + 1][j + 1]
-				);
-				weighted_neighb *= (1 - _index_macro(p->conductivity, i - 1, j - 1));
-				(*temp_tmp)[i][j] = (*temp_init)[i][j] * _index_macro(p->conductivity, i - 1, j - 1);
-				(*temp_tmp)[i][j] += weighted_neighb;
-				if(fabs((*temp_init)[i][j] - (*temp_tmp)[i][j]) >  maxdiff)
-					maxdiff = fabs((*temp_init)[i][j] - (*temp_tmp)[i][j]);
-			}
-		}
-		// syncrhonizing init matrix and temporary one
-		// for(int i = 0; i < N; ++i)
-		// 	memcpy( &(*temp_init)[i + 1][1], &(*temp_tmp)[i][0], M * sizeof(double));
-		#pragma omp single
-		{
-			maxdiff_vect2 =max_from_vect(maxdiff_vect, thread_num)  ;
-			tmp_reg = _mm_max_pd(_mm256_extractf128_pd(maxdiff_vect2, 0), _mm256_extractf128_pd(maxdiff_vect2, 1));
-			_mm_storeu_pd(maxdiff_vect_mem, tmp_reg);
-			maxdiff = max(maxdiff, max(maxdiff_vect_mem[0], maxdiff_vect_mem[1]));
-			++iter;
-		}
+			// finally start computations
+			#pragma omp for reduction (max: maxdiff)
+			for(int i = 1; i <= N; ++i){
+				int thread_id = omp_get_thread_num();
+				for(int j = 1; j <= (M - M % elems_per_iter) ; j += elems_per_iter){
+					cond_reg = _mm256_loadu_pd(&_index_macro(p->conductivity, i - 1, j - 1));
+					rev_cond_reg = _mm256_sub_pd(const1_reg, cond_reg);
 
-		#pragma omp single nowait
-		{
-			double* tmp_tmp = temp_init;
-			temp_init = temp_tmp;
-			temp_tmp = tmp_tmp;
-		}
+					temp_init_reg = _mm256_loadu_pd(&(*temp_init)[i][j]);
+					temp_init_reg_clone = temp_init_reg; //_mm256_loadu_pd(&(*temp_init)[i][j]);
+					temp_init_reg_left = _mm256_loadu_pd(&(*temp_init)[i][j - 1]);
+					temp_init_reg_right = _mm256_loadu_pd(&(*temp_init)[i][j + 1]);
+					temp_init_reg_top = _mm256_loadu_pd(&(*temp_init)[i - 1][j]);
 
-			if(iter % p->period == 0){
-				double local_sum = 0;
-				r->tmin = r->tmax = (*temp_tmp)[1][1];
-				maxdiff_vect2 = _mm256_set1_pd((*temp_tmp)[1][1]);
-				temp_init_reg_clone = _mm256_set1_pd((*temp_tmp)[1][1]);
-				direct_sum = _mm256_set1_pd(0);
-				for(int i = 1; i <= N; ++i){
-					for(int j = 1; j <= M - (M % elems_per_iter) ; j += elems_per_iter){
-						temp_init_reg =_mm256_loadu_pd(&(*temp_init)[i][j]);
-						direct_sum = _mm256_add_pd(direct_sum, temp_init_reg);
-						maxdiff_vect2 = _mm256_max_pd(maxdiff_vect2, temp_init_reg);
-						temp_init_reg_clone = _mm256_min_pd(temp_init_reg_clone, temp_init_reg);
-					}
+					temp_init_reg_d1 = _mm256_loadu_pd(&(*temp_init)[i - 1][j - 1]);
+					temp_init_reg_d3 = _mm256_loadu_pd(&(*temp_init)[i - 1][j + 1]);
 
-					for(int j = M - (M % elems_per_iter) + 1; j <= M ; ++j){
-						if((*temp_init)[i][j] > r->tmax)
-							r->tmax = (*temp_init)[i][j];
-						if((*temp_init)[i][j] < r->tmin)
-							r->tmin = (*temp_init)[i][j];
-						local_sum += (*temp_init)[i][j];
-					}
+					temp_init_reg_bottom = _mm256_loadu_pd(&(*temp_init)[i + 1][j]);
+
+					temp_init_reg_d4 = _mm256_loadu_pd(&(*temp_init)[i + 1][j + 1]);
+					temp_init_reg_d2 = _mm256_loadu_pd(&(*temp_init)[i + 1][j - 1]);
+
+					direct_sum = _mm256_add_pd(temp_init_reg_left, temp_init_reg_right);
+					direct_sum = _mm256_add_pd(direct_sum, temp_init_reg_top);
+					direct_sum = _mm256_add_pd(direct_sum, temp_init_reg_bottom);
+					direct_sum = _mm256_mul_pd(direct_sum, coef1_reg);
+
+					diag_sum = _mm256_add_pd(temp_init_reg_d1, temp_init_reg_d2);
+					diag_sum = _mm256_add_pd(diag_sum, temp_init_reg_d3);
+					diag_sum = _mm256_add_pd(diag_sum, temp_init_reg_d4);
+					diag_sum = _mm256_mul_pd(diag_sum, coef2_reg);
+
+					diag_sum = _mm256_add_pd(diag_sum, direct_sum);
+					diag_sum = _mm256_mul_pd(diag_sum, rev_cond_reg);
+					temp_init_reg = _mm256_mul_pd(temp_init_reg, cond_reg);
+					temp_init_reg = _mm256_add_pd(temp_init_reg, diag_sum);
+					_mm256_storeu_pd(&(*temp_tmp)[i][j], temp_init_reg);
+					double_temp_reg = _mm256_sub_pd(temp_init_reg, temp_init_reg_clone);
+						maxdiff_vect[thread_id] = _mm256_max_pd(maxdiff_vect[thread_id], _mm256_max_pd(_mm256_sub_pd(_mm256_set1_pd(0.0), double_temp_reg), double_temp_reg));
 				}
 
+				for(int j = (M - M % elems_per_iter) + 1; j <= M ; ++j){
+					double weighted_neighb = dir_nc *
+					( // Direct neighbors
+						(*temp_init)[i - 1][j] + (*temp_init)[i][j - 1] +
+						(*temp_init)[i + 1][j] + (*temp_init)[i][j + 1]
+					) + dig_nc *
+					( // Diagonal neighbors
+						(*temp_init)[i - 1][j - 1] + (*temp_init)[i + 1][j - 1] +
+						(*temp_init)[i - 1][j + 1] + (*temp_init)[i + 1][j + 1]
+					);
+					weighted_neighb *= (1 - _index_macro(p->conductivity, i - 1, j - 1));
+					(*temp_tmp)[i][j] = (*temp_init)[i][j] * _index_macro(p->conductivity, i - 1, j - 1);
+					(*temp_tmp)[i][j] += weighted_neighb;
+					if(fabs((*temp_init)[i][j] - (*temp_tmp)[i][j]) >  maxdiff)
+						maxdiff = fabs((*temp_init)[i][j] - (*temp_tmp)[i][j]);
+				}
+			}
+			// syncrhonizing init matrix and temporary one
+			// for(int i = 0; i < N; ++i)
+			// 	memcpy( &(*temp_init)[i + 1][1], &(*temp_tmp)[i][0], M * sizeof(double));
+			#pragma omp single
+			{
+				maxdiff_vect2 =max_from_vect(maxdiff_vect, thread_num)  ;
 				tmp_reg = _mm_max_pd(_mm256_extractf128_pd(maxdiff_vect2, 0), _mm256_extractf128_pd(maxdiff_vect2, 1));
 				_mm_storeu_pd(maxdiff_vect_mem, tmp_reg);
-				r->tmax = max(r->tmax, max(maxdiff_vect_mem[0], maxdiff_vect_mem[1]));
-
-				tmp_reg = _mm_min_pd(_mm256_extractf128_pd(temp_init_reg_clone, 0), _mm256_extractf128_pd(temp_init_reg_clone, 1));
-				_mm_storeu_pd(maxdiff_vect_mem, tmp_reg);
-				r->tmin = min(r->tmin, min(maxdiff_vect_mem[0], maxdiff_vect_mem[1]));
-
-				tmp_reg = _mm_add_pd(_mm256_extractf128_pd(direct_sum, 0), _mm256_extractf128_pd(direct_sum, 1));
-				_mm_storeu_pd(maxdiff_vect_mem, tmp_reg);
-
-				gettimeofday(&end, 0);
-				r->time = (end.tv_sec + (end.tv_usec / 1000000.0)) - (start.tv_sec + (start.tv_usec / 1000000.0));
-				r->niter = iter;
-				r->tavg = (local_sum + maxdiff_vect_mem[0] + maxdiff_vect_mem[1]) /(N * M);
-				r->maxdiff = maxdiff;
-				report_results(p, r);
+				maxdiff = max(maxdiff, max(maxdiff_vect_mem[0], maxdiff_vect_mem[1]));
+				++iter;
 			}
 
+			#pragma omp single nowait
+			{
+				double* tmp_tmp = temp_init;
+				temp_init = temp_tmp;
+				temp_tmp = tmp_tmp;
+			}
+
+				if(iter % p->period == 0){
+					double local_sum = 0;
+					r->tmin = r->tmax = (*temp_tmp)[1][1];
+					maxdiff_vect2 = _mm256_set1_pd((*temp_tmp)[1][1]);
+					temp_init_reg_clone = _mm256_set1_pd((*temp_tmp)[1][1]);
+					direct_sum = _mm256_set1_pd(0);
+					#pragma omp for
+					for(int i = 1; i <= N; ++i){
+						for(int j = 1; j <= M - (M % elems_per_iter) ; j += elems_per_iter){
+							temp_init_reg =_mm256_loadu_pd(&(*temp_init)[i][j]);
+							direct_sum = _mm256_add_pd(direct_sum, temp_init_reg);
+							maxdiff_vect2 = _mm256_max_pd(maxdiff_vect2, temp_init_reg);
+							temp_init_reg_clone = _mm256_min_pd(temp_init_reg_clone, temp_init_reg);
+						}
+
+						for(int j = M - (M % elems_per_iter) + 1; j <= M ; ++j){
+							if((*temp_init)[i][j] > r->tmax)
+								r->tmax = (*temp_init)[i][j];
+							if((*temp_init)[i][j] < r->tmin)
+								r->tmin = (*temp_init)[i][j];
+							local_sum += (*temp_init)[i][j];
+						}
+					}
+
+					tmp_reg = _mm_max_pd(_mm256_extractf128_pd(maxdiff_vect2, 0), _mm256_extractf128_pd(maxdiff_vect2, 1));
+					_mm_storeu_pd(maxdiff_vect_mem, tmp_reg);
+					r->tmax = max(r->tmax, max(maxdiff_vect_mem[0], maxdiff_vect_mem[1]));
+
+					tmp_reg = _mm_min_pd(_mm256_extractf128_pd(temp_init_reg_clone, 0), _mm256_extractf128_pd(temp_init_reg_clone, 1));
+					_mm_storeu_pd(maxdiff_vect_mem, tmp_reg);
+					r->tmin = min(r->tmin, min(maxdiff_vect_mem[0], maxdiff_vect_mem[1]));
+
+					tmp_reg = _mm_add_pd(_mm256_extractf128_pd(direct_sum, 0), _mm256_extractf128_pd(direct_sum, 1));
+					_mm_storeu_pd(maxdiff_vect_mem, tmp_reg);
+
+					gettimeofday(&end, 0);
+					r->time = (end.tv_sec + (end.tv_usec / 1000000.0)) - (start.tv_sec + (start.tv_usec / 1000000.0));
+					r->niter = iter;
+					r->tavg = (local_sum + maxdiff_vect_mem[0] + maxdiff_vect_mem[1]) /(N * M);
+					r->maxdiff = maxdiff;
+					#pragma omp single nowait
+					report_results(p, r);
+				}
+
+		}
+
 	}
+
 
 	r->tmin = r->tmax = (*temp_tmp)[1][1];
 	maxdiff_vect2 = _mm256_set1_pd((*temp_tmp)[1][1]);
--- assignment_2/heat_simd_sse4_big/compute.c
+++ assignment_2/heat_simd_sse4_big/compute.c
@@ -50,15 +50,6 @@ void do_compute(const struct parameters* p, struct results *r)
 	__m128d weighted_neighb_reg;
 	__m128d temp_init_reg;
 	__m128d temp_init_reg_clone;
-	__m128d temp_init_reg_left;
-	__m128d temp_init_reg_right;
-	__m128d temp_init_reg_top;
-	__m128d temp_init_reg_d1;
-	__m128d temp_init_reg_d2;
-	__m128d temp_init_reg_d3;
-	__m128d temp_init_reg_d4;
-	__m128d temp_init_reg_bottom;
-	__m128d temp_init_reg_bottom_clone;
 	__m128d maxdiff_vect[thread_num];
 	__m128d maxdiff_vect2;
 	__m128d tmp_reg;
@@ -98,15 +89,6 @@ void do_compute(const struct parameters* p, struct results *r)
 		weighted_neighb_reg, \
 		temp_init_reg, \
 		temp_init_reg_clone, \
-		temp_init_reg_left, \
-		temp_init_reg_right, \
-		temp_init_reg_top, \
-		temp_init_reg_d1, \
-		temp_init_reg_d2, \
-		temp_init_reg_d3, \
-		temp_init_reg_d4, \
-		temp_init_reg_bottom, \
-		temp_init_reg_bottom_clone, \
 		tmp_reg, \
 		direct_sum, \
 		diag_sum, \
@@ -114,143 +96,153 @@ void do_compute(const struct parameters* p, struct results *r)
 		rev_cond_reg, \
 		double_temp_reg\
 	)
-	while(iter < p->maxiter && maxdiff > p->threshold){
-		int thread_id = omp_get_thread_num();
-		// do computations;
-		#pragma omp barrier
-		maxdiff = 0.0;
-		maxdiff_vect[thread_id] = _mm_set1_pd(0.0);
-		// update most left and most right columns( cache suffers )
-		#pragma omp for
-		for(int i = 0; i < N; ++i){
-			(*temp_init)[i + 1][0] = (*temp_init)[i + 1][M]; // move last column to 0's
-			(*temp_init)[i + 1][M + 1] = (*temp_init)[i + 1][1]; // move first column to (M+1)'s
-			(*temp_tmp)[i + 1][0] = (*temp_init)[i + 1][M]; // move last column to 0's
-			(*temp_tmp)[i + 1][M + 1] = (*temp_init)[i + 1][1]; // move first column to (M+1)'s
-		}
+	{
+		__m128d temp_init_reg_left;
+		__m128d temp_init_reg_right;
+		__m128d temp_init_reg_top;
+		__m128d temp_init_reg_d1;
+		__m128d temp_init_reg_d2;
+		__m128d temp_init_reg_d3;
+		__m128d temp_init_reg_d4;
+		__m128d temp_init_reg_bottom;
 
-		// finally start computations
-		#pragma omp for reduction (max: maxdiff)
-		for(int i = 1; i <= N; ++i){
-			for(int j = 1; j <= (M - M % elems_per_iter) ; j += elems_per_iter){
-				cond_reg = _mm_loadu_pd(&_index_macro(p->conductivity, i - 1, j - 1));
-				rev_cond_reg = _mm_sub_pd(const1_reg, cond_reg);
 
-				temp_init_reg = _mm_loadu_pd(&(*temp_init)[i][j]);
-				temp_init_reg_clone = temp_init_reg; //_mm_loadu_pd(&(*temp_init)[i][j]);
-				temp_init_reg_left = _mm_loadu_pd(&(*temp_init)[i][j - 1]);
-				temp_init_reg_right = _mm_loadu_pd(&(*temp_init)[i][j + 1]);
-				temp_init_reg_top = _mm_loadu_pd(&(*temp_init)[i - 1][j]);
-
-				temp_init_reg_d1 = _mm_loadu_pd(&(*temp_init)[i - 1][j - 1]);
-				temp_init_reg_d3 = _mm_loadu_pd(&(*temp_init)[i - 1][j + 1]);
-
-				temp_init_reg_bottom = _mm_loadu_pd(&(*temp_init)[i + 1][j]);
-				temp_init_reg_bottom_clone = temp_init_reg_bottom; //_mm_loadu_pd(&(*temp_init)[i + 1][j]);
-
-				temp_init_reg_d4 = _mm_loadu_pd(&(*temp_init)[i + 1][j + 1]);
-				temp_init_reg_d2 = _mm_loadu_pd(&(*temp_init)[i + 1][j - 1]);
-
-				direct_sum = _mm_add_pd(temp_init_reg_left, temp_init_reg_right);
-				direct_sum = _mm_add_pd(direct_sum, temp_init_reg_top);
-				direct_sum = _mm_add_pd(direct_sum, temp_init_reg_bottom);
-				direct_sum = _mm_mul_pd(direct_sum, coef1_reg);
-
-				diag_sum = _mm_add_pd(temp_init_reg_d1, temp_init_reg_d2);
-				diag_sum = _mm_add_pd(diag_sum, temp_init_reg_d3);
-				diag_sum = _mm_add_pd(diag_sum, temp_init_reg_d4);
-				diag_sum = _mm_mul_pd(diag_sum, coef2_reg);
-
-				diag_sum = _mm_add_pd(diag_sum, direct_sum);
-				diag_sum = _mm_mul_pd(diag_sum, rev_cond_reg);
-				temp_init_reg = _mm_mul_pd(temp_init_reg, cond_reg);
-				temp_init_reg = _mm_add_pd(temp_init_reg, diag_sum);
-				_mm_storeu_pd(&(*temp_tmp)[i][j], temp_init_reg);
-				// for( int elem_i = 0 ; elem_i < elems_per_iter; ++elem_i){
-				// 	if(maxdiff < fabs((*temp_tmp)[i][j + elem_i] - (*temp_init)[i][j + elem_i]))
-				// 		maxdiff = fabs((*temp_tmp)[i][j + elem_i] - (*temp_init)[i][j + elem_i]);
-				// }
-
-				double_temp_reg = _mm_sub_pd(temp_init_reg, temp_init_reg_clone);
-				maxdiff_vect[thread_id] = _mm_max_pd(maxdiff_vect[thread_id], _mm_max_pd(_mm_sub_pd(_mm_set1_pd(0.0), double_temp_reg), double_temp_reg));
-			}
-
-			for(int j = (M - M % elems_per_iter) + 1; j <= M ; ++j){
-				double weighted_neighb = dir_nc *
-				( // Direct neighbors
-					(*temp_init)[i - 1][j] + (*temp_init)[i][j - 1] +
-					(*temp_init)[i + 1][j] + (*temp_init)[i][j + 1]
-				) + dig_nc *
-				( // Diagonal neighbors
-					(*temp_init)[i - 1][j - 1] + (*temp_init)[i + 1][j - 1] +
-					(*temp_init)[i - 1][j + 1] + (*temp_init)[i + 1][j + 1]
-				);
-				weighted_neighb *= (1 - _index_macro(p->conductivity, i - 1, j - 1));
-				(*temp_tmp)[i][j] = (*temp_init)[i][j] * _index_macro(p->conductivity, i - 1, j - 1);
-				(*temp_tmp)[i][j] += weighted_neighb;
-				if(fabs((*temp_init)[i][j] - (*temp_tmp)[i][j]) >  maxdiff)
-					maxdiff = fabs((*temp_init)[i][j] - (*temp_tmp)[i][j]);
-			}
-		}
-		// syncrhonizing init matrix and temporary one
-		// for(int i = 0; i < N; ++i)
-		// 	memcpy( &(*temp_init)[i + 1][1], &(*temp_tmp)[i][0], M * sizeof(double));
-		#pragma omp single
-		{
-			++iter;
-			maxdiff_vect2 =max_from_vect(maxdiff_vect, thread_num)  ;
-			_mm_storeu_pd(maxdiff_vect_mem, maxdiff_vect2);
-			maxdiff = max(maxdiff, max(maxdiff_vect_mem[0], maxdiff_vect_mem[1]));
-			gettimeofday(&end, 0);
-		}
-		#pragma omp single nowait
-		{
-			double* tmp_tmp = temp_init;
-			temp_init = temp_tmp;
-			temp_tmp = tmp_tmp;
-		}
-		// #pragma omp single nowait
-
-		if(iter % p->period == 0){
-			double local_sum = 0;
-			r->tmin = r->tmax = (*temp_tmp)[1][1];
-			maxdiff_vect2 = _mm_set1_pd((*temp_tmp)[1][1]);
-			temp_init_reg_clone = _mm_set1_pd((*temp_tmp)[1][1]);
-			direct_sum = _mm_set1_pd(0);
+		while(iter < p->maxiter && maxdiff > p->threshold){
+			int thread_id = omp_get_thread_num();
+			// do computations;
+			// update most left and most right columns( cache suffers )
 			#pragma omp for
+			for(int i = 0; i < N; ++i){
+				(*temp_init)[i + 1][0] = (*temp_init)[i + 1][M]; // move last column to 0's
+				(*temp_init)[i + 1][M + 1] = (*temp_init)[i + 1][1]; // move first column to (M+1)'s
+				(*temp_tmp)[i + 1][0] = (*temp_init)[i + 1][M]; // move last column to 0's
+				(*temp_tmp)[i + 1][M + 1] = (*temp_init)[i + 1][1]; // move first column to (M+1)'s
+			}
+			maxdiff = 0.0;
+			maxdiff_vect[thread_id] = _mm_set1_pd(0.0);
+			// finally start computations
+			#pragma omp for reduction (max: maxdiff)
 			for(int i = 1; i <= N; ++i){
-				for(int j = 1; j <= M - (M % elems_per_iter) ; j += elems_per_iter){
-					temp_init_reg =_mm_loadu_pd(&(*temp_init)[i][j]);
-					direct_sum = _mm_add_pd(direct_sum, temp_init_reg);
-					maxdiff_vect2 = _mm_max_pd(maxdiff_vect2, temp_init_reg);
-					temp_init_reg_clone = _mm_min_pd(temp_init_reg_clone, temp_init_reg);
+				for(int j = 1; j <= (M - M % elems_per_iter) ; j += elems_per_iter){
+					cond_reg = _mm_loadu_pd(&_index_macro(p->conductivity, i - 1, j - 1));
+					rev_cond_reg = _mm_sub_pd(const1_reg, cond_reg);
+
+					temp_init_reg = _mm_loadu_pd(&(*temp_init)[i][j]);
+					temp_init_reg_clone = temp_init_reg; //_mm_loadu_pd(&(*temp_init)[i][j]);
+					temp_init_reg_left = _mm_loadu_pd(&(*temp_init)[i][j - 1]);
+					temp_init_reg_right = _mm_loadu_pd(&(*temp_init)[i][j + 1]);
+					temp_init_reg_top = _mm_loadu_pd(&(*temp_init)[i - 1][j]);
+
+					temp_init_reg_d1 = _mm_loadu_pd(&(*temp_init)[i - 1][j - 1]);
+					temp_init_reg_d3 = _mm_loadu_pd(&(*temp_init)[i - 1][j + 1]);
+
+					temp_init_reg_bottom = _mm_loadu_pd(&(*temp_init)[i + 1][j]);
+					temp_init_reg_d4 = _mm_loadu_pd(&(*temp_init)[i + 1][j + 1]);
+					temp_init_reg_d2 = _mm_loadu_pd(&(*temp_init)[i + 1][j - 1]);
+
+					direct_sum = _mm_add_pd(temp_init_reg_left, temp_init_reg_right);
+					direct_sum = _mm_add_pd(direct_sum, temp_init_reg_top);
+					direct_sum = _mm_add_pd(direct_sum, temp_init_reg_bottom);
+					direct_sum = _mm_mul_pd(direct_sum, coef1_reg);
+
+					diag_sum = _mm_add_pd(temp_init_reg_d1, temp_init_reg_d2);
+					diag_sum = _mm_add_pd(diag_sum, temp_init_reg_d3);
+					diag_sum = _mm_add_pd(diag_sum, temp_init_reg_d4);
+					diag_sum = _mm_mul_pd(diag_sum, coef2_reg);
+
+					diag_sum = _mm_add_pd(diag_sum, direct_sum);
+					diag_sum = _mm_mul_pd(diag_sum, rev_cond_reg);
+					temp_init_reg = _mm_mul_pd(temp_init_reg, cond_reg);
+					temp_init_reg = _mm_add_pd(temp_init_reg, diag_sum);
+					_mm_storeu_pd(&(*temp_tmp)[i][j], temp_init_reg);
+					// for( int elem_i = 0 ; elem_i < elems_per_iter; ++elem_i){
+					// 	if(maxdiff < fabs((*temp_tmp)[i][j + elem_i] - (*temp_init)[i][j + elem_i]))
+					// 		maxdiff = fabs((*temp_tmp)[i][j + elem_i] - (*temp_init)[i][j + elem_i]);
+					// }
+
+					double_temp_reg = _mm_sub_pd(temp_init_reg, temp_init_reg_clone);
+					maxdiff_vect[thread_id] = _mm_max_pd(maxdiff_vect[thread_id], _mm_max_pd(_mm_sub_pd(_mm_set1_pd(0.0), double_temp_reg), double_temp_reg));
 				}
 
-				for(int j = M - (M % elems_per_iter) + 1; j <= M ; ++j){
-					if((*temp_init)[i][j] > r->tmax)
-						r->tmax = (*temp_init)[i][j];
-					if((*temp_init)[i][j] < r->tmin)
-						r->tmin = (*temp_init)[i][j];
-					local_sum += (*temp_init)[i][j];
+				for(int j = (M - M % elems_per_iter) + 1; j <= M ; ++j){
+					double weighted_neighb = dir_nc *
+					( // Direct neighbors
+						(*temp_init)[i - 1][j] + (*temp_init)[i][j - 1] +
+						(*temp_init)[i + 1][j] + (*temp_init)[i][j + 1]
+					) + dig_nc *
+					( // Diagonal neighbors
+						(*temp_init)[i - 1][j - 1] + (*temp_init)[i + 1][j - 1] +
+						(*temp_init)[i - 1][j + 1] + (*temp_init)[i + 1][j + 1]
+					);
+					weighted_neighb *= (1 - _index_macro(p->conductivity, i - 1, j - 1));
+					(*temp_tmp)[i][j] = (*temp_init)[i][j] * _index_macro(p->conductivity, i - 1, j - 1);
+					(*temp_tmp)[i][j] += weighted_neighb;
+					if(fabs((*temp_init)[i][j] - (*temp_tmp)[i][j]) >  maxdiff)
+						maxdiff = fabs((*temp_init)[i][j] - (*temp_tmp)[i][j]);
 				}
 			}
-
-			_mm_storeu_pd(maxdiff_vect_mem, maxdiff_vect2);
-			r->tmax = max(r->tmax, max(maxdiff_vect_mem[0], maxdiff_vect_mem[1]));
-
-			_mm_storeu_pd(maxdiff_vect_mem, temp_init_reg_clone);
-			r->tmin = min(r->tmin, min(maxdiff_vect_mem[0], maxdiff_vect_mem[1]));
-
-			_mm_storeu_pd(maxdiff_vect_mem, direct_sum);
-			r->time = (end.tv_sec + (end.tv_usec / 1000000.0)) - (start.tv_sec + (start.tv_usec / 1000000.0));
-			r->niter = iter;
-			r->tavg = (local_sum + maxdiff_vect_mem[0] + maxdiff_vect_mem[1]) /(N * M);
-			r->maxdiff = maxdiff;
+			// syncrhonizing init matrix and temporary one
+			// for(int i = 0; i < N; ++i)
+			// 	memcpy( &(*temp_init)[i + 1][1], &(*temp_tmp)[i][0], M * sizeof(double));
+			#pragma omp single
+			{
+				++iter;
+				maxdiff_vect2 =max_from_vect(maxdiff_vect, thread_num)  ;
+				_mm_storeu_pd(maxdiff_vect_mem, maxdiff_vect2);
+				maxdiff = max(maxdiff, max(maxdiff_vect_mem[0], maxdiff_vect_mem[1]));
+			}
 			#pragma omp single nowait
-			report_results(p, r);
+			{
+				double* tmp_tmp = temp_init;
+				temp_init = temp_tmp;
+				temp_tmp = tmp_tmp;
+			}
+			// #pragma omp single nowait
+
+			if(iter % p->period == 0){
+				double local_sum = 0;
+				r->tmin = r->tmax = (*temp_tmp)[1][1];
+				maxdiff_vect2 = _mm_set1_pd((*temp_tmp)[1][1]);
+				temp_init_reg_clone = _mm_set1_pd((*temp_tmp)[1][1]);
+				direct_sum = _mm_set1_pd(0);
+				#pragma omp for
+				for(int i = 1; i <= N; ++i){
+					for(int j = 1; j <= M - (M % elems_per_iter) ; j += elems_per_iter){
+						temp_init_reg =_mm_loadu_pd(&(*temp_init)[i][j]);
+						direct_sum = _mm_add_pd(direct_sum, temp_init_reg);
+						maxdiff_vect2 = _mm_max_pd(maxdiff_vect2, temp_init_reg);
+						temp_init_reg_clone = _mm_min_pd(temp_init_reg_clone, temp_init_reg);
+					}
+
+					for(int j = M - (M % elems_per_iter) + 1; j <= M ; ++j){
+						if((*temp_init)[i][j] > r->tmax)
+							r->tmax = (*temp_init)[i][j];
+						if((*temp_init)[i][j] < r->tmin)
+							r->tmin = (*temp_init)[i][j];
+						local_sum += (*temp_init)[i][j];
+					}
+				}
+
+				_mm_storeu_pd(maxdiff_vect_mem, maxdiff_vect2);
+				r->tmax = max(r->tmax, max(maxdiff_vect_mem[0], maxdiff_vect_mem[1]));
+
+				_mm_storeu_pd(maxdiff_vect_mem, temp_init_reg_clone);
+				r->tmin = min(r->tmin, min(maxdiff_vect_mem[0], maxdiff_vect_mem[1]));
+
+				_mm_storeu_pd(maxdiff_vect_mem, direct_sum);
+				gettimeofday(&end, 0);
+				r->time = (end.tv_sec + (end.tv_usec / 1000000.0)) - (start.tv_sec + (start.tv_usec / 1000000.0));
+				r->niter = iter;
+				r->tavg = (local_sum + maxdiff_vect_mem[0] + maxdiff_vect_mem[1]) /(N * M);
+				r->maxdiff = maxdiff;
+				#pragma omp single nowait
+				report_results(p, r);
+			}
+
 		}
 
 	}
+
 
 	gettimeofday(&end, 0);
 	r->tmin = r->tmax = (*temp_tmp)[1][1];
